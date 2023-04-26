19692624cda39b6463905ecced492fd7b04ec99b
codestudent24 <belgorod2407@yandex.ru>
2023-04-26 21:32:38 +1000

    feat: add language en-rus switch

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-const rows = [
+let lang = 'en';
+
+const rows_en = [
     ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
     ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del'],
     ['CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter'],
     ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '<i class="fa-solid fa-caret-up"></i>', 'Shift'],
     ['Ctrl', 'Win', 'Alt', '', 'Alt', '<i class="fa-solid fa-caret-left"></i>', '<i class="fa-solid fa-caret-down"></i>', '<i class="fa-solid fa-caret-right"></i>', 'Ctrl']
+];
+
+const rows_rus = [
+    ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
+    ['Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'Del'],
+    ['CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', "э", 'Enter'],
+    ['Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '<i class="fa-solid fa-caret-up"></i>', 'Shift'],
+    ['Ctrl', 'Win', 'Alt', '', 'Alt', '<i class="fa-solid fa-caret-left"></i>', '<i class="fa-solid fa-caret-down"></i>', '<i class="fa-solid fa-caret-right"></i>', 'Ctrl']
+];
+
+const codes = [
+    ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace'],
+    ['Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'racketRight', 'Backslash', 'Delete'],
+    ['CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', "Quote", 'Enter'],
+    ['ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight'],
+    ['ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight']
 ];
 
 function addRow(parent) {
@@ -25,6 +43,25 @@ function printText(event) {
     textArea.innerHTML+=event.target.textContent;
 }
 
+function switchLanguage() {
+    if (lang === 'en') {
+        for (let i=1; i<4; i++) {
+            for (let j=0; j<rows_rus[i].length; j++) {
+                buttonRows[i].children[j].textContent=rows_rus[i][j];
+            }
+        }
+        lang = 'rus';
+    } else {
+        for (let i=1; i<4; i++) {
+            for (let j=1; j<rows_en[i].length; j++) {
+                buttonRows[i].children[j].textContent=rows_en[i][j];
+            }
+        }
+        lang = 'en';
+        }
+    buttonRows[3].children[11].innerHTML = '<i class="fa-solid fa-caret-up"></i>';
+    }
+
 const title = document.createElement('h1');
 title.textContent = 'RSS Virtual Keyboard';
 title.classList.add('title');
@@ -41,12 +78,14 @@ document.querySelector('body').appendChild(keyboard)
 
 for (let i=0; i<5; i++) {
     const row = addRow(keyboard);
-    for (elem of rows[i]) {
+    for (elem of rows_en[i]) {
         addButton(row, elem)
     }
 }
 
+const buttonRows = document.querySelectorAll('.row');
 const buttons = document.querySelectorAll('.button');
+
 for (elem of buttons) {
     if (elem.textContent === 'Shift' ||
     elem.textContent === 'Enter' ||
